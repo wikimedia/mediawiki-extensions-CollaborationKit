45611f2d1c0d967fdaa444af9d9194e292c05033
Isarra <zhorishna@gmail.com>
2016-09-19 21:44:50 +0000

    Made a separate ToC and Icon classes for collaborationhubs and collaborationkit in general, respectively.
    Icons are generally static and called to render from the canned or onwiki
    inmages.
    ToC is called as part of the Hub rendering process.
    
    Icons probably need to be called more places, or stuff. Also we may need a js
    version, given that it's js anyway. But this is there, now.
    
    bug: T140170
    Change-Id: I3126a963c603c2bd4ca38b0552eb52e14697eaa2

---
+++ includes/content/CollaborationHubTOC.php
@@ -0,0 +1,219 @@
+<?php
+
+class CollaborationHubTOC {
+
+	/** @var $tocLinks array ids/links for ToC items that have been used already */
+	protected $tocLinks;
+
+	/**
+	 * Get unique id for ToC Link/header
+	 * @param $header
+	 * @return string
+	 */
+	public function getToCLinkID( $header ) {
+		$link = Sanitizer::escapeId( htmlspecialchars( $header ) );
+		$link2 = $link;
+		$linkCounter = 1;
+		while ( in_array( $link2, $this->tocLinks ) ) {
+			$link2 = $link . '_' . $linkCounter;
+			$spPageLinkCounter++;
+		}
+		$this->tocLinks[] = $link2;
+		return $link2;
+	}
+
+	public function resetToCLinks() {
+		$this->tocLinks = [];
+	}
+
+	public function __construct() {
+		$this->resetToCLinks();
+	}
+
+	/**
+	 * ToC rendering for hub
+	 * @param array $content block from collaborationhub
+	 * @param string $colour variable from collaborationhub content
+	 * @return string html
+	 */
+	public function renderToC( $content, $colour ) {
+		global $wgParser;
+		$linkRenderer = $wgParser->getLinkRenderer();
+
+		$html = Html::openElement( 'div', [ 'class' => 'wp-toc-container' ] );
+		$html .= Html::rawElement(
+			'div',
+			[ 'class' => 'toc-label' ],
+			wfMessage( 'collaborationkit-hub-toc-label' )->inContentLanguage()->text()
+		);
+		$html .= Html::openElement( 'ul' );
+
+		foreach ( $content as $item ) {
+			$title = Title::newFromText( $item['title'] );
+			$rev = Revision::newFromTitle( $title );
+
+			// TODO sanitise?
+			if ( isset( $item['display_title'] ) ) {
+				$displayTitle = $item['display_title'];
+			} else {
+				$displayTitle = $title->getSubpageText();
+			}
+
+			if ( isset( $item['image'] ) ) {
+				$displayIcon = CollaborationKitIcon::makeIconOrImage( $item['image'], 50, $colour );
+			} else {
+				$displayIcon = CollaborationKitIcon::makeIconOrImage( $displayTitle, 50, $colour );
+			}
+
+			$linkTarget = Title::newFromText( '#' . $this->getToCLinkID( $displayTitle ) );
+			$linkDisplay = new HtmlArmor( Html::rawElement(
+				'div',
+				[],
+				$displayIcon . Html::element( 'span', [ 'class' => 'item-label' ], $displayTitle )
+			) );
+			$link = $linkRenderer->makeLink( $linkTarget, $linkDisplay );
+
+			$html .= Html::rawElement(
+				'li',
+				[ 'class' => 'wp-toc-item' ],
+				$link
+			);
+		}
+
+		$html .= Html::closeElement( 'ul' );
+		$html .= Html::closeElement( 'div' );
+		return $html;
+	}
+
+	/**
+	 * ToC rendering for non-hubs
+	 * @param Title $title of hub the ToC is generated off
+	 *
+	 *
+	 * @return string html
+	 */
+	public function renderSubpageToC( $title ) {
+
+	}
+
+	// TOC stuff as was. This is going away.
+
+	/**
+	 * Helper function for fillParserOutput; return HTML for a ToC.
+	 * @param Title $title for target
+	 * @param string $type: main or flat or stuff (used as css class)
+	 * @return string|null
+	 */
+	protected function generateToC( Title $title, ParserOutput &$output, $type = 'main' ) {
+		// TODO use correct version of text; support PREVIEWS as well as just pulling the content revision
+		$rev = Revision::newFromTitle( $title );
+		if ( isset( $rev ) ) {
+			$sourceContent = $rev->getContent();
+			$html = '';
+
+			if ( $rev->getContentModel() == 'CollaborationHubContent' ) {
+				$ToCItems = [];
+
+				// Add project mainpage to toc for subpages
+				if ( $type != 'main' ) {
+
+					$display = Html::element(
+						'span',
+						[],
+						$sourceContent->getPageName()
+					);
+					$display = $sourceContent->getImage( 'puzzlepiece', 40 ) . $display;
+
+					$ToCItems[$sourceContent->getPageName()] = [
+						Html::rawElement(
+							'span',
+							[ 'class' => 'wp-toc-projectlabel' ],
+							wfMessage( 'collaborationhub-toc-partof' )->inContentLanguage()->text()
+						) . Linker::Link( $title, $display ),
+						'toc-mainpage'
+					];
+				}
+
+				foreach ( $sourceContent->getContent() as $item ) {
+					$spTitle = Title::newFromText( $item['item'] );
+					$spRev = Revision::newFromTitle( $spTitle );
+
+					if ( isset( $spRev ) ) {
+						$spContent = $spRev->getContent();
+						$spContentModel = $spRev->getContentModel();
+
+						$output->addTemplate( $spTitle, $spTitle->getArticleId(), $spRev->getId() );
+					} else {
+						$spContentModel = 'none';
+
+						$output->addTemplate( $spTitle, $spTitle->getArticleId(), null );
+					}
+
+					// Display name and #id
+					$item = $spContentModel == 'CollaborationHubContent' ?
+						$spContent->getPageName() : $spTitle->getSubpageText();
+					$display = Html::element( 'span', [ 'class' => 'item-label' ], $item );
+					while ( isset( $ToCItems[$item] ) ) {
+						// Already exists, add a 1 to the end to avoid duplicates
+						$item = $item . '1';
+					}
+
+					// Link
+					if ( $type != 'main' ) {
+						// TODO add 'selected' class if already on it
+						$link = $spTitle;
+					} else {
+						$link = Title::newFromText( '#' . htmlspecialchars( $item ) );
+					}
+
+					// Icon
+					if ( $spContentModel == 'CollaborationHubContent' /* && image is set in $spContent */ ) {
+						$display = $spContent->getImage( 'random', 50 ) . $display;
+					} else {
+						// Use this one as a surrogate because it's not a real hub page; $link can act as seed
+						$display = $this->getImage( 'random', 50, $item ) . $display;
+					}
+
+					$ToCItems[$item] = [ Linker::Link( $link, $display ), Sanitizer::escapeId( 'toc-' . $spTitle->getSubpageText() ) ];
+				}
+				$html .= Html::openElement( 'div', [ 'class' => 'wp-toc' ] );
+
+				if ( $type == 'main' ) {
+					$html .= Html::rawElement(
+						'div',
+						[ 'class' => 'toc-label' ],
+						wfMessage( 'collaborationkit-toc-label' )->inContentLanguage()->text()
+					);
+				}
+
+				$html .= Html::openElement( 'ul' );
+
+				foreach ( $ToCItems as $item => $linkJunk ) {
+					$html .= Html::rawElement(
+						'li',
+						[
+							'class' => 'wp-toc-item ' . $linkJunk[1] // id info
+						],
+						$linkJunk[0] // link html string
+					);
+				}
+				$html .= Html::closeElement( 'ul' );
+				$html .= '<div class="visualClear"></div>';
+				$html .= Html::closeElement( 'div' );
+
+				$html = Html::rawElement(
+					'div',
+					[ 'class' => 'wp-toc-container' ],
+					$html
+				);
+			} else {
+				$html = 'Page not found, ToC not possible';
+			}
+		} else {
+			$html = '';
+		}
+
+		return $html;
+	}
+
+}
---
+++ includes/content/CollaborationKitIcon.php
@@ -0,0 +1,154 @@
+<?php
+
+class CollaborationKitIcon {
+
+	/**
+	 * Generate an in icon based on an on-wiki file or a canned CK icon
+	 * @param string $icon icon id, filename, or random seed
+	 * @param int $size intended height/width for rendered icon in px
+	 * @param string $fallback what to do for no icon; allowed values are 'random', 'none', or a valid icon id
+	 * @return string html
+	 */
+	public static function makeIconOrImage( $icon, $size = 50, $colour = 'black', $fallback = 'random' ) {
+		// We were going to only find files with file name extensions, but that's hard to parse, and there's no way to really handle ones that aren't uploaded, so we'll just look and see if they're uploaded and be done with it.
+		if ( wfFindFile( $icon ) ) {
+			// TODO also find if prefixed with 'file:', 'image:', etc
+			return CollaborationKitIcon::makeImage( $icon, $size );
+		} elseif ( $fallback == 'none' ) {
+			return '';
+		} else {
+			// canned icons time
+
+			$iconsPreset = CollaborationKitIcon::getCannedIcons();
+
+			if ( !in_array( $icon, $iconsPreset ) && in_array( $fallback, $iconsPreset ) ) {
+				return CollaborationKitIcon::makeIcon( $fallback, $size, 'grey2', '#eee' );
+			} else {
+				// makeicon falls back to making a random icon anyway, and we've ruled out all the other fallbacks at this point
+				return CollaborationKitIcon::makeIcon( $icon, $size, $colour );
+			}
+		}
+	}
+
+	/**
+	 * Generate an in icon using a canned CK icon
+	 * @param string $icon icon id or random seed
+	 * @param int $size intended height/width for rendered icon in px
+	 * @param string $fallback what to do for no icon; allowed values are 'random', 'none', or a valid icon id
+	 * @return string html
+	 */
+	public static function makeIcon( $icon, $size = 50, $colour = 'black', $background = 'transparent' ) {
+		$iconsPreset = CollaborationKitIcon::getCannedIcons();
+
+		if ( in_array( $icon, $iconsPreset ) ) {
+			$iconClass = Sanitizer::escapeClass( $icon );
+		} else {
+			// Random time
+			// Choose class name using $icon value as seed
+			$iconClass = $iconsPreset[ hexdec( sha1( $icon )[0] ) % 27];
+		}
+
+		if ( $colour == 'black' ) {
+			$colorSuffix = '';
+		} else {
+			$colorSuffix = '-' . $colour;
+		}
+		return Html::element(
+			'div',
+			[
+				'class' => [
+					'mw-ckicon',
+					'mw-ckicon-' . $iconClass .  $colorSuffix
+				],
+				'css' => "height: {$size}px; width: {$size}px; background-color: $background;"
+			]
+		);
+	}
+
+	/**
+	 * Make an image from a file onwiki
+	 * Assumes the file exists, and this was already checked. Doesn't work if it doesn't.
+	 * @param string $file filename
+	 * @param int $size width, height in px
+	 * @param string $background colour for optional css backgroundstuff
+	 * @return string html
+	 */
+	public static function makeImage( $file, $size = 50, $background = 'transparent' ) {
+		return Html::rawElement(
+			'div',
+			[
+				'class' => 'file-image',
+				'css' => "max-height: {$size}px; background-color: $background;"
+			],
+			wfFindFile( $file )->transform( [ 'width' => $size ] )->toHtml()
+		);
+	}
+
+	/**
+	 * @return array of stupidly many icons
+	 */
+	public static function getCannedIcons() {
+		// Keep this synced with the icons listed in the module in extension.json
+		return $iconsPreset = [
+			// Randomly selectable items
+			'book',
+			'circlestar',
+			'clock',
+			'community',
+			'contents',
+			'die',
+			'edit',
+			'eye',
+			'flag',
+			'funnel',
+			'gear',
+			'heart',
+			'journal',
+			'key',
+			'link',
+			'map',
+			'menu',
+			'newspaper',
+			'ol',
+			'page',
+			'paperclip',
+			'puzzlepiece',
+			'ribbon',
+			'rocket',
+			'star',
+			'sun',
+			'ul',
+
+			'addimage',
+			'addmapmarker',
+			'addquote',
+			'bell',
+			'circleline',
+			'circletriangle',
+			'circlex',
+			'discussion',
+			'download',
+			'editprotected',
+			'gallery',
+			'image',
+			'lock',
+			'mail',
+			'mapmarker',
+			'message',
+			'messagenew',
+			'messagescary',
+			'move',
+			'nowiki',
+			'pagechecked',
+			'pageribbon',
+			'pagesearch',
+			'print',
+			'quotes',
+			'search',
+			'starmenu',
+			'translate',
+			'trash',
+			'user'
+		];
+	}
+}
