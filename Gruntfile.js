78ac225e6408a126d0fd54543bcfae7b5b3b41ff
Umherirrender <umherirrender_de.wp@web.de>
2017-06-14 15:00:32 +0200

    Add stylelint for less files
    Fixed the following rules:
    - number-leading-zero
    - string-quotes
    - declaration-property-value-blacklist
    - max-empty-lines
    - selector-list-comma-newline-after
    - selector-pseudo-class-parentheses-space-inside
    - color-named
    
    Change-Id: I7ad4be738be5cdf1c27cbc100a8e737fb169e6a2

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,6 +4,7 @@ module.exports = function ( grunt ) {
 	grunt.loadNpmTasks( 'grunt-jsonlint' );
 	grunt.loadNpmTasks( 'grunt-banana-checker' );
 	grunt.loadNpmTasks( 'grunt-jscs' );
+	grunt.loadNpmTasks( 'grunt-stylelint' );
 
 	grunt.initConfig( {
 		jshint: {
@@ -26,12 +27,19 @@ module.exports = function ( grunt ) {
 			all: [
 				'*.json',
 				'**/*.json',
+				'.stylelintrc',
 				'!node_modules/**',
 				'!vendor/**'
+			]
+		},
+		stylelint: {
+			all: [
+				'**/*.less',
+				'!node_modules/**'
 			]
 		}
 	} );
 
-	grunt.registerTask( 'test', [ 'jshint', 'jscs', 'jsonlint', 'banana' ] );
+	grunt.registerTask( 'test', [ 'jshint', 'jscs', 'jsonlint', 'banana', 'stylelint' ] );
 	grunt.registerTask( 'default', 'test' );
 };
